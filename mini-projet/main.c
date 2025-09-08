eb3fa5dfec3fdb77fbae71f8f8f5ef6edca80d32
anwar962 <elmousaouyanwar31@gmail.com>
2025-09-08 11:46:00 +0200

    code

---
+++ mini-projet/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char T[hello];
+    char[-];
+    char lettre;
+    int nbrtentative;
+
+    printf("entrez les elements du tableau");
+    scanf("%c",&T[hello]);
+    printf("entrez les(-)du tableau");
+    T[0]='-';
+    T[1]='-';
+    T[2]='-';
+    T[3]='-';
+    T[4]='-';
+    scanf("%c",T[-]);
+    printf("entrez une lettre:");
+    scanf("%c",&lettre);
+    for(i=0;i<5;i++){
+        if(T[-]=lettre){
+            printf("un mot chacher est reveler");
+        }
+        else if
+            printf("vous avez entrez une mauvaise reponse");
+        else("bravo vous avez un mot passe valide");
+        nbrtentative=1
+        while(nbrtentative>5);
+        printf("game over");
+        nbr++
+        return 0;
+    }
+
+
+
+
+    return 0;
+}
